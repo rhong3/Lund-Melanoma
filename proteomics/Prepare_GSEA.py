bc54b3c5afe3d58d4bc17876add428fdcd7bd48e
rh2740 <rh2740@nyu.edu>
2019-03-25 17:15:41 -0400

    4

--- proteomics/Prepare_GSEA.py
+++ proteomics/Prepare_GSEA.py
@@ -1,15 +1,14 @@
 import pandas as pd
 
-ica = pd.read_csv("./ICA/Initial/Clean_proteomics_IC_centroid.txt", sep='\t', header=0, index_col=0)
+ica = pd.read_csv("./ICA/Gene_level/J_Clean_proteomics_IC_centroid.txt", sep='\t', header=0, index_col=0)
 
 wna_clinical = pd.read_csv('./wna_clinical.tsv', sep='\t', header=0, index_col=0)
 
-proteomics = pd.read_csv('./Clean_proteomics.tsv', sep='\t', header=0, index_col=0)
+proteomics = pd.read_csv('./J_Clean_proteomics.tsv', sep='\t', header=0, index_col=0)
 
-ica = ica.sort_values(by=['62'], ascending=False)
+ica = ica.sort_values(by=['77'])
 
-wna_clinical = wna_clinical.sort_values(by=['5 year survival from primary diagnosis ((Date death-date prim diagn)-1825 days). Days differing from 5 years',
-                                            '5 year survival from first metastasis ((Date death-date first met)-1825 days). Days differing from 5 years'])
+wna_clinical = wna_clinical.sort_values(by=['local_Cutaneous', 'local_Lymph node', 'local_Subcutaneous', 'local_Visceral'])
 
 ind = list(wna_clinical.index)
 
